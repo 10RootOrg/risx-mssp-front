f3c5eaae83cd37256daf14eff96a30829b7084af
D-V-W <david.v.wolfson@gmail.com>
2024-07-17 14:51:00 +0300

    add func to menu

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -25,7 +25,9 @@ function Settings({
   set_visblePage("Settings");
 
   const { all_Tools, front_URL } = useContext(GeneralContext);
-  console.log(`https://${front_URL.replace("http://", "")}/portainer`,"555555555555555555555555555555555555555555555555555555555555555555");
+
+  const [MenuSelector, setMenuSelector] = useState("Config Files");
+
   //  show sidebar in this page
   useEffect(() => {
     if (show_SideBar === false) {
@@ -69,39 +71,51 @@ function Settings({
         <div className="resource-group-top-boxes mb-c"></div>
 
         <div className="mb-c">
-          <Settings_Menu />{" "}
+          <Settings_Menu setMenuSelector={setMenuSelector} MenuSelector={MenuSelector} />{" "}
         </div>
 
         <div style={{ display: "flex", flexDirection: "column", gap: "45px" }}>
-          <Settings_section_config />
+          {MenuSelector == "Config Files" && <Settings_section_config />}
 
-          <Settings_section_ShowInUi all_Tools={all_Tools} />
+          {MenuSelector == "UI Settings" && (
+            <Settings_section_ShowInUi all_Tools={all_Tools} />
+          )}
 
-          <Settings_section_edit_mssp_config_json all_Tools={all_Tools} />
+          {MenuSelector == "Modules paths" && (
+            <Settings_section_edit_mssp_config_json all_Tools={all_Tools} />
+          )}
 
-          <Settings_section_process
-            isMainProcessWork={isMainProcessWork}
-            set_isMainProcessWork={set_isMainProcessWork}
-          />
-          <PortAiner />
-          <Settings_section_logs
-            usethis={"log_mssp_backend"}
-            fileName={"msspBack.log"}
-            headline={"MSSP Backend"}
-            subline={"Node JS backend"}
-          />
-          <Settings_section_logs
-            usethis={"log_python_main"}
-            fileName={"main.log"}
-            headline={"Python Interval"}
-            subline={"Python Interval log"}
-          />
-          <Settings_section_logs
-            usethis={"log_python_interval"}
-            fileName={"interval.log"}
-            headline={"Python main"}
-            subline={"Active Now"}
-          />
+          {MenuSelector == "Output Sample" && (
+            <Settings_section_process
+              isMainProcessWork={isMainProcessWork}
+              set_isMainProcessWork={set_isMainProcessWork}
+            />
+          )}
+          {MenuSelector == "Portainer" && <PortAiner />}
+          {MenuSelector == "Backend Log" && (
+            <Settings_section_logs
+              usethis={"log_mssp_backend"}
+              fileName={"msspBack.log"}
+              headline={"MSSP Backend"}
+              subline={"Node JS backend"}
+            />
+          )}
+          {MenuSelector == "Python Main Log" && (
+            <Settings_section_logs
+              usethis={"log_python_main"}
+              fileName={"main.log"}
+              headline={"Python Main"}
+              subline={"Python Main log"}
+            />
+          )}
+          {MenuSelector == "Python Interval Log" && (
+            <Settings_section_logs
+              usethis={"log_python_interval"}
+              fileName={"interval.log"}
+              headline={"Python Interval"}
+              subline={"Active Now"}
+            />
+          )}
         </div>
       </div>
     </>
